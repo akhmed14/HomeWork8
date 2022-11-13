9bc71d9ce2cf15fecbb9f26aa952e3d96f7a7f99
akhmed <akhmed@storyAkhmedMacBook-Air.local>
2022-11-13 03:04:56 +0300

    54

--- task54/Program.cs
+++ task54/Program.cs
@@ -22,7 +22,6 @@ PrintArray(array);
 
 void GetArray(int[,] array)
 {
-    var random = new Random();
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
